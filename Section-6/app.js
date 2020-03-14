d415ef0da5253db8a71f070d8a35d80eecadf39b
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-03-14 15:26:48 -0400

    completing section 54

--- Section-6/app.js
+++ Section-6/app.js
@@ -44,6 +44,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 };
 
+// Delete Book
+UI.prototype.deleteBook = function(target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -51,7 +58,7 @@ UI.prototype.clearFields = function() {
   document.getElementById('isbn').value = '';
 };
 
-// Event Listeners
+// Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
   const title = document.getElementById('title').value,
@@ -78,6 +85,21 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     // Clear fields
     ui.clearFields();
   }
+
+  e.preventDefault();
+});
+
+// Event Listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+  console.log(123);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
 });
