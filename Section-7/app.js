5a3ad040ca8d656fdc11064985bf3ab67b956af7
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-03-14 19:50:33 -0400

    completing section 60

--- Section-7/app.js
+++ Section-7/app.js
@@ -1,39 +1,110 @@
-document.getElementById('button').addEventListener('click', loadData);
+// ===================================================
+// SECTION 59: XHR Object Methods & Working With Text
+// ===================================================
 
-function loadData() {
-  // Create an XHR Object
+// document.getElementById('button').addEventListener('click', loadData);
+
+// function loadData() {
+//   // Create an XHR Object
+//   const xhr = new XMLHttpRequest();
+
+//   // OPEN
+//   xhr.open('GET', 'data.txt', true);
+
+//   //   console.log('READYSTATE', xhr.readyState);
+
+//   // Optional - Used for spinners/loaders
+//   xhr.onprogress = function() {
+//     console.log('READYSTATE', xhr.readyState);
+//   };
+
+//   xhr.onload = function() {
+//     console.log('READYSTATE', xhr.readyState);
+//     if (this.status === 200) {
+//       //   console.log(this.responseText);
+//       document.getElementById(
+//         'output'
+//       ).innerHTML = `<h1>${this.responseText}</h1>`;
+//     }
+//   };
+
+//   //   xhr.onreadystatechange = function() {
+//   //     console.log('READYSTATE', xhr.readyState);
+
+//   //     if (this.status === 200 && this.readyState === 4) {
+//   //       console.log(this.responseText);
+//   //     }
+//   //   };
+
+//   xhr.onerror = function() {
+//     console.log('Request error...');
+//   };
+
+//   xhr.send();
+// }
+
+// =============================================
+// SECTION 60: Working With Ajax & JSON
+// =============================================
+
+document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+// Load Single Customer
+function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
 
-  // OPEN
-  xhr.open('GET', 'data.txt', true);
-
-  //   console.log('READYSTATE', xhr.readyState);
-
-  // Optional - Used for spinners/loaders
-  xhr.onprogress = function() {
-    console.log('READYSTATE', xhr.readyState);
-  };
+  xhr.open('GET', 'customer.json', true);
 
   xhr.onload = function() {
-    console.log('READYSTATE', xhr.readyState);
     if (this.status === 200) {
       //   console.log(this.responseText);
-      document.getElementById(
-        'output'
-      ).innerHTML = `<h1>${this.responseText}</h1>`;
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+      <ul>
+        <li>ID: ${customer.id}</li>
+        <li>Name: ${customer.name}</li>
+        <li>Company: ${customer.company}</li>
+        <li>Phone: ${customer.phone}</li>
+      </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
     }
   };
 
-  //   xhr.onreadystatechange = function() {
-  //     console.log('READYSTATE', xhr.readyState);
+  xhr.send();
+}
 
-  //     if (this.status === 200 && this.readyState === 4) {
-  //       console.log(this.responseText);
-  //     }
-  //   };
+// Load Customers
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
 
-  xhr.onerror = function() {
-    console.log('Request error...');
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      //   console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+
+      customers.forEach(function(customer) {
+        output += `
+        <ul>
+            <li>ID: ${customer.id}</li>
+            <li>Name: ${customer.name}</li>
+            <li>Company: ${customer.company}</li>
+            <li>Phone: ${customer.phone}</li>
+        </ul>
+        `;
+      });
+
+      document.getElementById('customers').innerHTML = output;
+    }
   };
 
   xhr.send();
