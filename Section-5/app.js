4a49a5ccb6929a824e03610da088af0240b99656
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-03-14 12:54:30 -0400

    completing section 50

--- Section-5/app.js
+++ Section-5/app.js
@@ -218,36 +218,70 @@
 // Section 49: Classes
 // ================================================
 
+// class Person {
+//   constructor(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.birthday = new Date(dob);
+//   }
+
+//   greeting() {
+//     return `Hello there ${this.firstName} ${this.lastName}`;
+//   }
+
+//   calculateAge() {
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   }
+
+//   getsMarried(newLastName) {
+//     this.lastName = newLastName;
+//   }
+
+//   static addNumbers(x, y) {
+//     return x + y;
+//   }
+// }
+
+// const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+// mary.getsMarried('Thompson');
+
+// console.log(mary);
+
+// console.log(Person.addNumbers(1, 2));
+
+// ================================================
+// Section 50: Sub Classes
+// ================================================
+
 class Person {
-  constructor(firstName, lastName, dob) {
+  constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.birthday = new Date(dob);
   }
 
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
   }
+}
 
-  calculateAge() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
+
+    this.phone = phone;
+    this.membership = membership;
   }
 
-  getsMarried(newLastName) {
-    this.lastName = newLastName;
-  }
-
-  static addNumbers(x, y) {
-    return x + y;
+  static getMembershipCost() {
+    return 500;
   }
 }
 
-const mary = new Person('Mary', 'Williams', '11-13-1980');
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
 
-mary.getsMarried('Thompson');
+console.log(john.greeting());
 
-console.log(mary);
-
-console.log(Person.addNumbers(1, 2));
+console.log(Customer.getMembershipCost());
