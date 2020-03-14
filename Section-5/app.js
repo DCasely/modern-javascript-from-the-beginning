f9873065eb8f676f77e01f0a181bf6090389d169
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-03-14 12:46:55 -0400

    completing section 49

--- Section-5/app.js
+++ Section-5/app.js
@@ -186,30 +186,68 @@
 // Section 48: Using Object.create
 // ================================================
 
-const personPrototypes = {
-  greeting: function() {
+// const personPrototypes = {
+//   greeting: function() {
+//     return `Hello there ${this.firstName} ${this.lastName}`;
+//   },
+//   getsMarried: function(newLastName) {
+//     this.lastName = newLastName;
+//   }
+// };
+
+// const mary = Object.create(personPrototypes);
+// mary.firstName = 'Mary';
+// mary.lastName = 'Williams';
+// mary.age = 30;
+
+// mary.getsMarried('Thompson');
+
+// console.log(mary.greeting());
+
+// const brad = Object.create(personPrototypes, {
+//   firstName: { value: 'Brad' },
+//   lastName: { value: 'Traversy' },
+//   age: { value: 36 }
+// });
+
+// console.log(brad);
+
+// console.log(brad.greeting());
+
+// ================================================
+// Section 49: Classes
+// ================================================
+
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+
+  greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-  getsMarried: function(newLastName) {
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getsMarried(newLastName) {
     this.lastName = newLastName;
   }
-};
 
-const mary = Object.create(personPrototypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
+  static addNumbers(x, y) {
+    return x + y;
+  }
+}
+
+const mary = new Person('Mary', 'Williams', '11-13-1980');
 
 mary.getsMarried('Thompson');
 
-console.log(mary.greeting());
+console.log(mary);
 
-const brad = Object.create(personPrototypes, {
-  firstName: { value: 'Brad' },
-  lastName: { value: 'Traversy' },
-  age: { value: 36 }
-});
-
-console.log(brad);
-
-console.log(brad.greeting());
+console.log(Person.addNumbers(1, 2));
