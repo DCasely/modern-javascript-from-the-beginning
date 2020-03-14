a6c913558a578ba9fcfe6e02c4375a3f4c238170
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-03-14 19:19:57 -0400

    completing section 59

---
+++ Section-7/app.js
@@ -0,0 +1,40 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData() {
+  // Create an XHR Object
+  const xhr = new XMLHttpRequest();
+
+  // OPEN
+  xhr.open('GET', 'data.txt', true);
+
+  //   console.log('READYSTATE', xhr.readyState);
+
+  // Optional - Used for spinners/loaders
+  xhr.onprogress = function() {
+    console.log('READYSTATE', xhr.readyState);
+  };
+
+  xhr.onload = function() {
+    console.log('READYSTATE', xhr.readyState);
+    if (this.status === 200) {
+      //   console.log(this.responseText);
+      document.getElementById(
+        'output'
+      ).innerHTML = `<h1>${this.responseText}</h1>`;
+    }
+  };
+
+  //   xhr.onreadystatechange = function() {
+  //     console.log('READYSTATE', xhr.readyState);
+
+  //     if (this.status === 200 && this.readyState === 4) {
+  //       console.log(this.responseText);
+  //     }
+  //   };
+
+  xhr.onerror = function() {
+    console.log('Request error...');
+  };
+
+  xhr.send();
+}
