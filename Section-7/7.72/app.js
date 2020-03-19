845b560f16379a7b9f9afe28a64dea265382eb01
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-03-19 18:19:02 -0400

    completing section 72

--- Section-7/7.72/app.js
+++ Section-7/7.72/app.js
@@ -14,10 +14,10 @@ const data = {
 };
 
 // Create User
-http
-  .post('https://jsonplaceholder.typicode.com/users', data)
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+// http
+//   .post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
 // Update User
 // http
@@ -26,7 +26,7 @@ http
 //   .catch(err => console.log(err));
 
 // Delete User
-// http
-//   .delete('https://jsonplaceholder.typicode.com/users/2')
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
+http
+  .delete('https://jsonplaceholder.typicode.com/users/2')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
