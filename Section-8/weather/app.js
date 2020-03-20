eddf011ac95c2899283267b303d7ab39b0e29ce8
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-03-20 18:22:54 -0400

    completing Section 80

--- Section-8/weather/app.js
+++ Section-8/weather/app.js
@@ -1,5 +1,7 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
+// Init UI
+const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -10,7 +12,7 @@ function getWeather() {
   weather
     .getWeather()
     .then(results => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch(err => console.log(err));
 }
