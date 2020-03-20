b7db67fe8302371934aad17785fc8f519a4f0a8f
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-03-20 10:57:48 -0400

    completing Section 74

--- Section-8/app.js
+++ Section-8/app.js
@@ -1,1 +1,24 @@
-console.log('app connected');
+// Init Github
+const github = new Github();
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', e => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== '') {
+    // Make http call
+    github.getUser(userText).then(data => {
+      if (data.profile.message === 'Not Found') {
+        // Show alert
+      } else {
+        // Show profile
+      }
+    });
+  } else {
+    // Clear profile
+  }
+});
--- Section-8/gitjhub.js
+++ Section-8/gitjhub.js
@@ -1,1 +1,18 @@
-console.log('github connected');
+class Github {
+  constructor() {
+    this.client_id = 'd5a1daa152143de34360';
+    this.client_secret = '60fc9f211ae57de0f1ce287f9f0ed2131754d617';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
+    };
+  }
+}
