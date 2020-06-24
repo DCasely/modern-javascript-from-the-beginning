5f7b2a655f149292de2bb89474feac84f0bde8b8
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-24 12:42:02 -0400

    section 23

--- Section-3/js_sandbox/app.js
+++ Section-3/js_sandbox/app.js
@@ -2,45 +2,46 @@
 // 23. EXAMINING THE DOCUMENT OBJECT
 // ======================================
 
-// let val;
+let val;
 
-// val = document;
-// val = document.all;
-// val = document.all[2];
-// val = document.all.length;
-// val = document.head;
-// val = document.body;
-// val = document.doctype;
-// val = document.domain;
-// val = document.URL;
-// val = document.contentType;
+val = document;
+val = document.all;
+val = document.all[0];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
 
-// val = document.forms;
-// val = document.forms[0];
-// val = document.forms[0].id;
-// val = document.forms[0].method;
-// val = document.forms[0].action;
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
 
-// val = document.links;
-// val = document.links[0];
-// val = document.links[0].id;
-// val = document.links[0].className;
-// val = document.links[0].classList[0];
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].href;
+val = document.links[0].classList;
+val = document.links[0].classList[0];
 
-// val = document.images;
+val = document.images;
 
-// val = document.scripts;
-// val = document.scripts[2].getAttribute('src');
+val = document.scripts;
+val = document.scripts[0];
+val = document.scripts[2].getAttribute('src');
 
-// let scripts = document.scripts;
+const scripts = document.scripts;
+const scriptsArray = [...scripts];
 
-// let scriptsArr = Array.from(scripts);
+scriptsArray.map((scipt) => console.log(scipt.getAttribute('src')));
 
-// scriptsArr.forEach(function(script) {
-//   console.log(script.getAttribute('src'));
-// });
-
-// console.log(val);
+console.log(val);
 
 // ======================================
 // 24. DOM SELECTORS FOR SINGLE ELEMENTS
