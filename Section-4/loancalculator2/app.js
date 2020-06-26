2cc4b568c33086537387d912c34346009db01054
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-26 16:37:04 -0400

    section 39

--- Section-4/loancalculator2/app.js
+++ Section-4/loancalculator2/app.js
@@ -1,11 +1,35 @@
 const calculateBtn = document.getElementById('loan-form');
+const loader = document.getElementById('loading');
+const results = document.getElementById('results');
+const interestField = document.getElementById('interest');
+const amountField = document.getElementById('amount');
+const yearsField = document.getElementById('years');
+const reset = document.getElementById('reset');
 
 // LISTEN FOR SUBMIT
-calculateBtn.addEventListener('submit', calculateResults);
+calculateBtn.addEventListener('submit', delayResults);
+
+// DELAY SHOWING RESULTS
+function delayResults(e) {
+  // Show the Loading Image
+  loader.style.display = 'block';
+
+  // Hide the Loading Image after 3 secs and Show Results
+  setTimeout(function () {
+    // Hide Loader
+    loader.style.display = 'none';
+
+    // Calculate & Show Results
+    calculateResults(e);
+    results.style.display = 'block';
+    reset.style.display = 'block';
+  }, 1500);
+
+  e.preventDefault();
+}
 
 // CALCULATE RESULTS
 function calculateResults(e) {
-  console.log('Calculating...');
   //   UI Variables
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -52,11 +76,23 @@ function showError(error) {
   // Insert error above heading
   card.insertBefore(errorDiv, form);
 
-  // Clear error after 3 seconds
+  // Clear error after delay
   setTimeout(clearError, 3000);
 }
 
 // CLEAR ERROR
 function clearError() {
   document.querySelector('.alert').remove();
+}
+
+// RESET INITIAL STATE
+reset.addEventListener('click', resetState);
+
+function resetState() {
+  loader.style.display = 'none';
+  results.style.display = 'none';
+  reset.style.display = 'none';
+  amountField.value = '';
+  interestField.value = '';
+  yearsField.value = '';
 }
