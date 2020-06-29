e647c1e5246d9900b2f1afe5a7d23ffa42cacb27
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-06-29 14:28:56 -0400

    section 46

--- Section-5/app.js
+++ Section-5/app.js
@@ -33,72 +33,118 @@
 // SECTION 45: BUILT IN CONSTRUCTORS
 // ================================================
 
-// STRINGS
-const name1 = 'Davin';
-const name2 = new String('Davin');
+// // STRINGS
+// const name1 = 'Davin';
+// const name2 = new String('Davin');
 
-console.log(name1);
+// console.log(name1);
 
-name2.foo = 'bar';
-console.log(name2);
+// name2.foo = 'bar';
+// console.log(name2);
 
-console.log(typeof name1);
-console.log(typeof name2);
+// console.log(typeof name1);
+// console.log(typeof name2);
 
-if (name1 === 'Davin') {
-  console.log('Yes');
-} else {
-  console.log('No');
-}
+// if (name1 === 'Davin') {
+//   console.log('Yes');
+// } else {
+//   console.log('No');
+// }
 
-if (name2 === 'Davin') {
-  console.log('Yes');
-} else {
-  console.log('No');
-}
+// if (name2 === 'Davin') {
+//   console.log('Yes');
+// } else {
+//   console.log('No');
+// }
 
-// NUMBERS
-const num1 = 5;
-const num2 = new Number(5);
+// // NUMBERS
+// const num1 = 5;
+// const num2 = new Number(5);
 
-console.log(typeof num1);
-console.log(typeof num2);
+// console.log(typeof num1);
+// console.log(typeof num2);
 
-const bool1 = true;
-const bool2 = new Boolean(true);
+// const bool1 = true;
+// const bool2 = new Boolean(true);
 
-console.log(typeof bool1);
-console.log(typeof bool2);
+// console.log(typeof bool1);
+// console.log(typeof bool2);
 
-// FUNCTIONS
+// // FUNCTIONS
 
-const getSum1 = function (x, y) {
-  return x + y;
-};
+// const getSum1 = function (x, y) {
+//   return x + y;
+// };
 
-console.log(getSum1(1, 1));
+// console.log(getSum1(1, 1));
 
-const getSum2 = new Function('x', 'y', 'return 1 + 1');
+// const getSum2 = new Function('x', 'y', 'return 1 + 1');
 
-console.log(getSum2(1, 1));
+// console.log(getSum2(1, 1));
 
-// OBJECTS
-const john = { name: 'John' };
-console.log(john);
+// // OBJECTS
+// const john = { name: 'John' };
+// console.log(john);
 
-const john2 = new Object({ name: 'John' });
-console.log(john2);
+// const john2 = new Object({ name: 'John' });
+// console.log(john2);
 
-// ARRAYS
-const arr1 = [1, 2, 3, 4];
-const arr2 = new Array(1, 2, 3, 4);
+// // ARRAYS
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = new Array(1, 2, 3, 4);
 
-console.log(arr1);
-console.log(arr2);
+// console.log(arr1);
+// console.log(arr2);
 
-// REGEX (REGULAR EXPRESSIONS)
-const re1 = /\w+/;
-const re2 = new RegExp('\\w+');
+// // REGEX (REGULAR EXPRESSIONS)
+// const re1 = /\w+/;
+// const re2 = new RegExp('\\w+');
 
-console.log(re1);
-console.log(re2);
+// console.log(re1);
+// console.log(re2);
+
+// ================================================
+// SECTION 46: PROTOTYPES EXPLAINED
+// ================================================
+
+// // Object.prototype
+// // Person.prototype
+
+// function Person(firstName, lastName, dob) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.birthday = new Date(dob);
+// }
+
+// // Calculate age
+// Person.prototype.calculateAge = function () {
+//   const diff = Date.now() - this.birthday.getTime();
+//   const ageDate = new Date(diff);
+//   return Math.abs(ageDate.getUTCFullYear() - 1970);
+// };
+
+// // Get full name
+// Person.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// };
+
+// // Gets Married
+// Person.prototype.getsMarried = function (newLastName) {
+//   this.lastName = newLastName;
+// };
+
+// const john = new Person('John', 'Doe', '8-12-90');
+// const mary = new Person('Mary', 'Jane', 'March 5, 1988');
+
+// console.log(john);
+// console.log(mary);
+
+// console.log(john.getFullName());
+// console.log(mary.getFullName());
+
+// mary.getsMarried('Smith');
+// console.log(mary.getFullName());
+
+// console.log(mary.hasOwnProperty('firstName'));
+// console.log(mary.hasOwnProperty('getFullName'));
+// console.log(mary.__proto__.hasOwnProperty('getFullName'));
